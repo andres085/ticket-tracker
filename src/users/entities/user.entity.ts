7192b4ba5dbea46b3debf6b83ada47023823a0d7
andres085 <andresito.martinez1985@gmail.com>
2023-08-21 11:16:28 -0300

    configured the user entity

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,1 +1,28 @@
-export class User {}
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+export enum UserRole {
+  USER = 'user',
+  SUPPORT = 'support',
+  ADMIN = 'admin'
+}
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column('text', { unique: true })
+  username: string;
+
+  @Column('text', { unique: true })
+  password: string;
+
+  @Column('text')
+  email: string;
+
+  @Column('enum', {
+    enum: UserRole,
+    default: UserRole.USER
+  })
+  role: UserRole;
+}
