f22603fb388e2029c86b3928be8bc43a089d56ae
andres085 <andresito.martinez1985@gmail.com>
2023-08-27 12:18:37 -0300

    add validation to the create user dto

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,26 @@
-export class CreateUserDto {}
+import { IsEmail, IsEnum, IsString, Matches, MaxLength, MinLength } from "class-validator";
+import { UserRole } from "../entities/user.entity";
+
+export class CreateUserDto {
+  @IsString()
+  @MinLength(3)
+  username: string;
+
+  @IsString()
+  @MinLength(6)
+  @MaxLength(12)
+  @Matches(/(?:(?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+    message:
+      'The password must have a Uppercase, lowercase letter and a number',
+  })
+  password: string;
+
+  @IsString()
+  @IsEmail()
+  email: string;
+
+  @IsEnum(UserRole, {
+    message: `Role must be one of the following: ${Object.values(UserRole).join(', ')}`
+  })
+  role: UserRole;
+} 
